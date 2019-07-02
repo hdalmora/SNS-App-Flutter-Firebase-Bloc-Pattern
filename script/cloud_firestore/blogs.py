6ca22e9b3ee2f1a2731fd2afbd1fded6430cd18c
Wiandono Saputro <wiandonosaputro@gmail.com>
2019-07-02 17:22:19 +0900

    Add a script to make dummy account

---
+++ script/cloud_firestore/blogs.py
@@ -0,0 +1,20 @@
+import firebase_admin
+from firebase_admin import credentials, firestore, auth
+from reselpy_randomizer import randomizer
+
+credentials = credentials.Certificate('./ServiceAccountKey.json')
+default_app = firebase_admin.initialize_app(credentials)
+db = firestore.client()
+
+for i in range(5):
+    doc_ref = db.collection(u'blogs').document()
+    doc_ref.set({
+        u'authoEmail': u'dal@email.com',
+        u'authorID': u'2zzLa9akToOdVK13pIuTiudy2za2',
+        u'content': randomizer.Randomizer.get_random_phrase(10),
+        u'likesCounter': 0,
+        u'timestamp': firestore.firestore.SERVER_TIMESTAMP,
+        u'title': randomizer.Randomizer.get_random_phrase(3)
+    })
+
+    print('Blog #' + str(i + 1) + ' OK')
